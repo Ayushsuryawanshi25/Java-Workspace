5194251332c7dca2afd48c58adb09c62780f426c
Ayush Suryawanshi <ayushsuryawanshi2507@gmail.com>
2025-12-18 11:17:47 +0530

    [Ayush Suryawanshi]Adding Second program

--- Week-1/Calculator.java
+++ Week-1/Calculator.java
@@ -4,41 +4,49 @@
 
 public class Calculator{
 
-public static int add(int a, int b){
-return a+b;
-}
+   public static int add(int a, int b){
+     return a+b;
+   }
 
-public static int sub(int a, int b){
-return a-b;
-}
+   public static int sub(int a, int b){
+     return a-b;
+   }
 
-public static int  multi(int a, int b){
-return a*b;
-}
+   public static int  multi(int a, int b){
+     return a*b;
+   }
+ 
+   public static int divi(int a, int b){
+     return a/b;
+   }
 
-public static int divi(int a, int b){
-return a/b;
-}
+   public static int  remain(int a, int b){
+     return a%b;
+   }
 
-public static int  remain(int a, int b){
-return a%b;
-}
-
-public static int  square(int a, int b){
-return a*a;
-}
+   public static int  square(int a, int b){
+     return a*a;
+   }
 
 
-public static void main(String [] args){
-System.out.print("enter two numbers");
-Scanner sc = new Scanner(System.in);
-int a = sc.nextInt();
-int b = sc.nextInt();
-System.out.print(add(a,b));
-System.out.print(sub(a,b));
-System.out.print(multi(a,b));
-System.out.print(divi(a,b));
-System.out.print(remain(a,b));
-System.out.print(square(a,b));
+   public static void main(String [] args){ 	
+    System.out.print("enter two numbers");
+	
+    Scanner sc = new Scanner(System.in);
+	
+    int a = sc.nextInt();
+    int b = sc.nextInt(); 
+	
+    System.out.println(add(a,b));
+	
+    System.out.println(sub(a,b));
+	
+    System.out.println(multi(a,b));
+	
+    System.out.println(divi(a,b));
+	
+    System.out.println(remain(a,b));
+	
+    System.out.println(square(a,b));
 }
 }
